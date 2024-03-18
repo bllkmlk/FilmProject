c3894750292d441612c87f98110b3165b179d9bd
Bilal KAMALAK <132837919+bllkmlk@users.noreply.github.com>
2024-03-19 02:20:46 +0300

    Add files via upload

---
+++ FilmProject-Prototype/project.js
@@ -0,0 +1,65 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+const cardBody = document.querySelectorAll(".card-body")[1];
+const clear = document.getElementById("clear-films");
+
+// UI Objesini Başlatma
+const ui = new UI();
+// Storage Objesi Üret
+
+const storage = new Storage();
+
+// Tüm Eventleri Yükleme
+
+eventListener();
+
+function eventListener() {
+  form.addEventListener("submit", addFilm);
+  document.addEventListener("DOMContentLoaded", function () {
+    let films = storage.getFilmsFromStorage();
+    ui.loadAllFilms(films);
+  });
+
+  cardBody.addEventListener("click", deleteFilm);
+
+  clear.addEventListener("click", clearAllFilms);
+}
+function addFilm(e) {
+  const title = titleElement.value;
+  const director = directorElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || director === "" || url === "") {
+    ui.displayMessage("Tüm alanları doldurun...", "danger");
+  } else {
+    // Yeni Film
+    const newFilm = new Film(title, director, url);
+
+    ui.addFilmToUI(newFilm);
+    storage.addFilmToStorage(newFilm); // Storage'a film eklme
+    ui.displayMessage("Film başarıyla eklendi...", "success");
+  }
+
+  ui.clearInputs(titleElement, urlElement, directorElement);
+
+  e.preventDefault();
+}
+function deleteFilm(e) {
+  if (e.target.id === "delete-film") {
+    ui.deleteFilmFromUI(e.target);
+    storage.deleteFilmFromStorage(
+      e.target.parentElement.previousElementSibling.previousElementSibling
+        .textContent
+    );
+
+    ui.displayMessage("Silme işlemi başarılı...", "success");
+  }
+}
+function clearAllFilms() {
+  if (confirm("Emin misiniz ?")) {
+    ui.clearAllFilmsFromUI();
+    storage.clearAllFilmsFromStorage();
+  }
+}
